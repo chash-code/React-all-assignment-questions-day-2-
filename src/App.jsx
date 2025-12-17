1e381f9fbabb4fbf3ef8aeeecfca5db6b1a1c527
chash-code <panchalchash@gmail.com>
2025-12-17 23:20:54 +0530

    Add Calculator update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Counter from './Counter';
+import Calculator from './Calculator';
 
 function App() {
   return (
     <div>
-      <Counter />
+      <h1 style={{ textAlign: 'center' }}>React Assignment</h1>
+      
+      <div style={{ border: '2px solid #ccc', margin: '20px', padding: '20px' }}>
+        <h2>Q1: Counter</h2>
+        <Counter />
+      </div>
+      
+      <div style={{ border: '2px solid #ccc', margin: '20px', padding: '20px' }}>
+        <h2>Q2: Calculator</h2>
+        <Calculator />
+      </div>
     </div>
   );
 }
